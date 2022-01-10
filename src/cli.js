b144bc71992eaeef842c9f0d3e66c55554969771
Лисов Павел <lisovpa@vtblife.ru>
2022-01-10 13:27:47 +0300

    feat(*): add eslint

--- src/cli.js
+++ src/cli.js
@@ -1,8 +1,8 @@
 import readlineSync from 'readline-sync';
 
-
+// eslint-disable-next-line import/prefer-default-export
 export const greetings = () => {
-  console.log('Welcome to the Brain Games!')
+  console.log('Welcome to the Brain Games!');
   const name = readlineSync.question('May I have your name?');
   console.log(`Hello, ${name}!`);
-}
+};
